b17efa7f7b1488e8e5e71eb74814d573d55b8540
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2023-07-08 19:05:14 +0300

    Sound manager & camera manager scenes

---
+++ Addons/GodotLibrary/Singletons/CameraManager/CameraManager.gd
@@ -0,0 +1,8 @@
+## Singleton to expose functionality to resources
+extends Node2D
+
+@export var main_camera_reference:ReferenceNodeResource
+
+func get_current_camera()->Node:
+	return get_viewport().get_camera_2d()
+	
