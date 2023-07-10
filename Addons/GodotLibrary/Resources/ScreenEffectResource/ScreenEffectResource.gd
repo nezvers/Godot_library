94f711928eac261fc1beee3aec3750ef26b0c1a9
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2023-07-10 23:25:37 +0300

    ScreenEffect singleton

---
+++ Addons/GodotLibrary/Resources/ScreenEffectResource/ScreenEffectResource.gd
@@ -0,0 +1,8 @@
+## Uses ScreenManager to get current camera.
+## Then spawn a scene as a child to the camera.
+class_name ScreenEffectResource
+extends Resource
+
+
+@export var tween_value_resource:TweenValueResource
+@export var screen_effect_scene:PackedScene
---
+++ Addons/GodotLibrary/Singletons/ScreenEffect/ScreenEffect.gd
@@ -0,0 +1,5 @@
+## Singleton used as parent for screen effects
+extends CanvasLayer
+
+func _ready()->void:
+	layer = 128
