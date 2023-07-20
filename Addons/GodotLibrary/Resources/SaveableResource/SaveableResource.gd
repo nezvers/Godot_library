797b5c8d820ddeb17ea20df573c15e53c2969597
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2023-07-20 23:43:56 +0300

    Initial managers

--- Addons/GodotLibrary/Resources/SaveableResource/SaveableResource.gd
+++ Addons/GodotLibrary/Resources/SaveableResource/SaveableResource.gd
@@ -2,6 +2,9 @@
 class_name SaveableResource
 extends Resource
 
+signal resource_saved
+signal resource_loaded
+
 ## Override function for resetting to default values
 func reset_resource()->void:
 	pass
@@ -11,7 +14,7 @@ func prepare_save()->Resource:
 	return self.duplicate()
 
 ## Override to ad logic for reading loaded data and applying to current instance of the Resource
-func prepare_load(data:Resource)->void:
+func prepare_load(_data:Resource)->void:
 	pass
 
 func get_save_file_path()->String:
@@ -21,6 +24,7 @@ func save_resource()->void:
 	if ResourceSaver.save(prepare_save(), get_save_file_path()):
 		print(resource_name, ": failed to save")
 		return
+	resource_saved.emit()
 
 func load_resource()->void:
 	if !FileAccess.file_exists(get_save_file_path()):
@@ -32,3 +36,9 @@ func load_resource()->void:
 		return
 	
 	prepare_load(data)
+	resource_loaded.emit()
+
+func delete_resource()->void:
+	if !FileAccess.file_exists(get_save_file_path()):
+		pass
+	DirAccess.remove_absolute(get_save_file_path())
