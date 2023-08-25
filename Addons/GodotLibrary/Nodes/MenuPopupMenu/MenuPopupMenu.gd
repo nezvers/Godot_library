75faef77d23c958e22713f8e35bb304e973d2838
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2023-08-25 18:06:30 +0300

    palette assets

--- Addons/GodotLibrary/Nodes/MenuPopupMenu/MenuPopupMenu.gd
+++ Addons/GodotLibrary/Nodes/MenuPopupMenu/MenuPopupMenu.gd
@@ -3,9 +3,15 @@ class_name MenuPopupMenu
 
 var callbacks:Dictionary = {}
 
+## Call super._ready() if inheriting
 func _ready()->void:
-	index_pressed.connect(on_index_pressed)
-	about_to_popup.connect(on_about_to_pop)
+	initialize()
+
+func initialize()->void:
+	if !index_pressed.is_connected(on_index_pressed):
+		index_pressed.connect(on_index_pressed)
+	if !index_pressed.is_connected(on_about_to_pop):
+		about_to_popup.connect(on_about_to_pop)
 
 func on_index_pressed(i:int)->void:
 	callbacks[i].call()
@@ -13,11 +19,12 @@ func on_index_pressed(i:int)->void:
 func submenu_popup(submenu:PopupMenu)->void:
 	add_submenu_item(submenu.name, submenu.name)
 
-func callback_button(item_name:String, callback:Callable)->void:
+func callback_button(item_name:String, callback:Callable, tooltip:String = "")->void:
 	var i: = item_count
 	add_item(item_name)
 	callbacks[i] = callback
-	pass
+	if !tooltip.is_empty():
+		set_item_tooltip(i, tooltip)
 
 func on_about_to_pop()->void:
 	var parent: = get_parent()
